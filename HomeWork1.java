46f0a6a365ceb30fd9140a0ac131c39fbe3ac9cd
Максим Овсієнко <47577556+MaksimOvsienko@users.noreply.github.com>
2019-02-16 21:21:22 +0200

    Add files via upload

---
+++ HomeWork1.java
@@ -0,0 +1,13 @@
+package com;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        System.out.println("**** ****     *     *     *");
+        System.out.println("*    *  *    * *    * * * *");
+        System.out.println("**** ****   *****   *  *  *");
+        System.out.println("*    *     *     *  *     *");
+        System.out.println("**** *    *       * *     *");
+//        System.out.println("");
+
+    }
+}
